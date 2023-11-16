221fb9d873984892ed2a06e73ac4a265180ee602
temirovazat <azatbekt5@gmail.com>
2023-11-16 19:28:34 +0600

    fix(users): iexact serializer filter invited_by_code

--- src/api/v1/users/serializers.py
+++ src/api/v1/users/serializers.py
@@ -34,7 +34,9 @@ class UserDetailsSerializer(UserSerializer):
 
         This method retrieves the list of users invited by the current user.
         """
-        invited_users = User.objects.filter(invited_by_code=obj.invite_code)
+        invited_users = User.objects.filter(
+            invited_by_code__iexact=obj.invite_code
+        )
         invited_serializer = InvitedUserSerializer(invited_users, many=True)
         return invited_serializer.data
 
